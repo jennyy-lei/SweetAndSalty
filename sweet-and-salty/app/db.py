893f14d622f2842c0700e0bba3d5bf2e0f67f86b
aeternalis1 <michaelhuang88@outlook.com>
2021-09-19 02:33:33 -0400

    db update

--- sweet-and-salty/app/db.py
+++ sweet-and-salty/app/db.py
@@ -132,8 +132,13 @@ def get_viable_recipes_rids(conn, iids):
 
 
 def get_completed_recipes_info(conn, ingredients):
+    if not ingredients:
+        return []
+    iid = get_iid(conn, ingredients[-1])
+    if iid == None:
+        return []
     iids = get_iids_by_inames(conn, ingredients)
-    viable_rids = get_viable_recipes_rids(conn, iids)
+    viable_rids = get_recipes_with_ingredient(conn, iid)
     info = get_bulk_recipe_info(conn, viable_rids)
 
     completed_recipes = []
@@ -150,6 +155,9 @@ def get_completed_recipes_info(conn, ingredients):
 
 
 def get_recommended_ingredients(conn, ingredients, shuffle_counter):
+    if not ingredients:
+        return ["milk", "rice", "eggs", "tomatoes", "chicken"]
+
     iids = get_iids_by_inames(conn, ingredients)
     viable_rids = get_viable_recipes_rids(conn, iids)
     viable_iids = {}
@@ -178,87 +186,4 @@ def get_recommended_ingredients(conn, ingredients, shuffle_counter):
 
     return get_inames_by_iids(conn, recommended_iids)
 
-
-def run_transaction(conn, op, max_retries=3):
-    """
-    Execute the operation *op(conn)* retrying serialization failure.
-
-    If the database returns an error asking to retry the transaction, retry it
-    *max_retries* times before giving up (and propagate it).
-    """
-    # leaving this block the transaction will commit or rollback
-    # (if leaving with an exception)
-    with conn:
-        for retry in range(1, max_retries + 1):
-            try:
-                print(op(conn))
-
-                # If we reach this point, we were able to commit, so we break
-                # from the retry loop.
-                return
-
-            except SerializationFailure as e:
-                # This is a retry error, so we roll back the current
-                # transaction and sleep for a bit before retrying. The
-                # sleep time increases for each failed transaction.
-                logging.debug("got error: %s", e)
-                conn.rollback()
-                logging.debug("EXECUTE SERIALIZATION_FAILURE BRANCH")
-                sleep_ms = (2 ** retry) * 0.1 * (random.random() + 0.5)
-                logging.debug("Sleeping %s seconds", sleep_ms)
-                time.sleep(sleep_ms)
-
-            except psycopg2.Error as e:
-                logging.debug("got error: %s", e)
-                logging.debug("EXECUTE NON-SERIALIZATION_FAILURE BRANCH")
-                raise e
-
-        raise ValueError(f"Transaction did not succeed after {max_retries} retries")
-
-
-def test_retry_loop(conn):
-    """
-    Cause a seralization error in the connection.
-
-    This function can be used to test retry logic.
-    """
-    with conn.cursor() as cur:
-        # The first statement in a transaction can be retried transparently on
-        # the server, so we need to add a dummy statement so that our
-        # force_retry() statement isn't the first one.
-        cur.execute("SELECT now()")
-        cur.execute("SELECT crdb_internal.force_retry('1s'::INTERVAL)")
-    logging.debug("test_retry_loop(): status message: %s", cur.statusmessage)
-
-
 conn_string = "postgres://testuser:testpassword@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/sweet-and-salty-3616.bank?sslmode=verify-full&sslrootcert=root.crt"
-
-def main():
-    conn = psycopg2.connect(conn_string)
-
-    test_ingr = ["milk", "flour"]
-    test_ingr2 = ["broccoli", "bacon", "green onions", "raisins", "sugar", "mayonnaise", "vinegar"]
-    print (get_completed_recipes_info(conn, test_ingr2))
-    print (get_recommended_ingredients(conn, test_ingr, 0))
-
-    '''
-    try:
-        run_transaction(conn, lambda conn: get_recommended_ingredients(conn, test_ingr, 0))
-
-        # The function below is used to test the transaction retry logic.  It
-        # can be deleted from production code.
-        # run_transaction(conn, test_retry_loop)
-    except ValueError as ve:
-        # Below, we print the error and continue on so this example is easy to
-        # run (and run, and run...).  In real code you should handle this error
-        # and any others thrown by the database interaction.
-        logging.debug("run_transaction(conn, op) failed: %s", ve)
-        pass
-    '''
-
-    # Close communication with the database.
-    conn.close()
-
-
-if __name__ == "__main__":
-    main()
