d09e9574b7239f065766dee60226c4ffc7697377
Vivian Liu <vivianliu1@live.com>
2021-09-19 01:15:47 -0400

    fix recipes list

--- sweet-and-salty/src/RecipeFinder/RecipeFinder.js
+++ sweet-and-salty/src/RecipeFinder/RecipeFinder.js
@@ -64,7 +64,7 @@ export default function RecipeFinder() {
     }
     
     const newRecommendedRecipes = (newRecommendedRecipes) => {
-        setRecipeList(newRecommendedRecipes + recipeList); 
+        setRecipeList(newRecommendedRecipes); 
     }
     
     return (
